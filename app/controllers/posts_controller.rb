d22936a40e2fa5961bc8d8a4562110a3facd4b25
adudley78 <dudleyadam@gmail.com>
2019-06-10 15:18:04 -0400

    Build app/views/posts/show.html.erb and update posts#create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,11 +13,10 @@ class PostsController < ApplicationController
     end
 
     def create
-        # render plain: params[:post].inspect
         @post = Post.new(post_params)
         @post.save
 
-        redirect_to @post
+        render :show
     end
 
 private
